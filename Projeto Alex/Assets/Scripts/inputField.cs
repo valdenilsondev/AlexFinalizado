40a741abc14bfbd2afd3d1122330f56ade945313
valdenilson barbosa <valdenilsondev@gmail.com>
2023-06-13 10:59:23 -0300

    Criar palavras cruzadas

--- Projeto Alex/Assets/Scripts/inputField.cs
+++ Projeto Alex/Assets/Scripts/inputField.cs
@@ -9,15 +9,47 @@ public class inputField : MonoBehaviour
 {
     
 
-    public TMP_InputField _inputField;
+    public TMP_InputField[] _inputField;
+    public int identificadorId;
+    public string palavraCompleta;
+    public bool isVerificar;
+    public List<int> idAtual;
+    public List<string> nomeCompleto;
+
+    
     
 
 
     public void inserir(string letra)
     {
-        _inputField.text = letra;
+
+        //_inputField[identificadorId].text = "casa";
+
+        _inputField[identificadorId].text = letra;
+
+        palavraCompleta += _inputField[identificadorId].text;
+       
+
+       
 
     }
+
+    public void identificador(int id)
+    {
+        identificadorId = id;
+        idAtual.Add(identificadorId);
+
+       
+
+    }
+
+    public void Checar()
+    {
+        
+        
+
+    }
+
 
 
 }
